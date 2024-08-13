5c4796f5c26307ea7e51d4ab883d3907280b8477
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-08-13 11:19:39 +0700

    Add nullsafety

--- src/main/kotlin/ExerciseController.kt
+++ src/main/kotlin/ExerciseController.kt
@@ -50,5 +50,18 @@ class ExerciseController {
         empGen.minSalary = 50
         empGen.maxSalary = 100
         println(empGen.generateRandomEmployee())
+
+        // Null safety
+        println((1..5).sumOf { id -> salaryById(id) })
     }
+
+    fun employeeById(id: Int) = when(id) {
+        1 -> Employee("Mary", 20)
+        2 -> null
+        3 -> Employee("John", 21)
+        4 -> Employee("Ann", 23)
+        else -> null
+    }
+
+    fun salaryById(id: Int) = employeeById(id)?.salary ?: 0
 }
